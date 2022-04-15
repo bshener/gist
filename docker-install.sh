af934a39f5e9b3d704ed8f0ff72974d089f552b8
bshener <bshener@yahoo.com>
2022-04-15 06:41:36 +0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -8,10 +8,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - &&\
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" &&\
 sudo apt update &\
 apt-cache policy docker-ce &&\
-
 sudo apt install -y docker-ce &&\
-sudo systemctl status docker &&\
-
 sudo usermod -aG docker ${USER} \
 
 echo "Done. 👍"
